3dbee72796f2c1e27f34ed7a69233f1db7683090
maddy392 <maddy392@gmail.com>
2024-04-14 15:36:28 -0500

    adding different segues for different buttons

--- Roshambo/PlayViewController.swift
+++ Roshambo/PlayViewController.swift
@@ -14,8 +14,7 @@ class PlayViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func playRoshambo(_ sender: UIButton) {
-        
+    @IBAction func playRock(_ sender: UIButton) {
         var controller: ResultsViewController
         
         controller = self.storyboard?.instantiateViewController(withIdentifier: "ResultsViewController") as! ResultsViewController
@@ -24,6 +23,34 @@ class PlayViewController: UIViewController {
         
         self.present(controller, animated: true, completion: nil)
         
+    }
+    
+    @IBAction func playPaper() {
+        
+        self.performSegue(withIdentifier: "playPaperSegue", sender: self)
+    }
+    
+    @IBAction func playScissor() {
+        self.performSegue(withIdentifier: "playScissorSegue", sender: self)
+    }
+//    @IBAction func playScissor() {
+//
+//    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "playPaperSegue" {
+            
+            let controller = segue.destination as!  ResultsViewController
+            
+            controller.playerSelection = 1
+//            print(sender)
+        }
+        else if segue.identifier == "playScissorSegue" {
+            let controller = segue.destination as!  ResultsViewController
+            
+            controller.playerSelection = 2
+//            print(sender)
+        }
     }
     
 }
